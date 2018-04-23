2f30f8000cd871d8aeb286c577f3260dc3fcbb61
HB04-23 <HB04-23@BIT04-19>
2018-04-23 19:32:18 +0900

    수정 테스트 해보기

--- ch5/src/Exercise6_22/Exercise6_22.java
+++ ch5/src/Exercise6_22/Exercise6_22.java
@@ -4,7 +4,7 @@ public class Exercise6_22 {
 	public static void main(String[] args) {
 		String str = "123";
 		System.out.println(str + "는 숫자입니까? " + isNumber(str));
-		str = "1234o";
+		str = "1234o1";
 		System.out.println(str + "는 숫자입니까? " + isNumber(str));
 	}
 
