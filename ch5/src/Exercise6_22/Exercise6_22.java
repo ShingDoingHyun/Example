c45fc6182b0d53b2797f994de26d353b39898037
HB04-23 <HB04-23@BIT04-19>
2018-04-23 19:43:32 +0900

    커밋 레이블 색상테스트

--- ch5/src/Exercise6_22/Exercise6_22.java
+++ ch5/src/Exercise6_22/Exercise6_22.java
@@ -2,7 +2,7 @@ package Exercise6_22;
 
 public class Exercise6_22 {
 	public static void main(String[] args) {
-		String str = "123";
+		String str = "12113";
 		System.out.println(str + "는 숫자입니까? " + isNumber(str));
 		str = "1234o1";
 		System.out.println(str + "는 숫자입니까? " + isNumber(str));
