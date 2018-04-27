0a3acf322b7831fc302bd31178b26be6dba77b02
HB04-23 <HB04-23@BIT04-19>
2018-04-27 16:32:48 +0900

    인터페이스 주석달기

--- ch7/src/repairableTest/RepairableTest.java
+++ ch7/src/repairableTest/RepairableTest.java
@@ -6,10 +6,10 @@ public class RepairableTest {
 		Dropship dropship = new Dropship();//드랍십객체~
 
 		// Marine marine = new Marine();
-		SCV scv = new SCV();
+		SCV scv = new SCV();	
 		scv.rapair(tank);	//scv의 리페어로 탱크 객체를 보내자~
 		scv.rapair(dropship);
-		// scv.rapair(marine);
+		// scv.rapair(marine);	//마린은 리페어러블 인터페이스에서 궁축되지 않아서 리페어가 불가능함
 	}
 
 }
@@ -89,9 +89,9 @@ class SCV extends GroundUnit implements Repairable {
 	}
 
 	public void rapair(Repairable r) {	//리페어러블 인터페이스로 구축된 애들
-		if (r instanceof Unit) {
-			Unit u = (Unit) r;	
-			while (u.hitPoint != u.MAX_HP) {
+		if (r instanceof Unit) {	//받은 객체가 유닛에게 상독되었는지 체크/유닛인지
+			Unit u = (Unit) r;		//유닛으로 형병환
+			while (u.hitPoint != u.MAX_HP) {	//피만땅으로 회복
 				u.hitPoint++;
 
 			}
