9b324c34d46e58cf9e409ae9045d05d90f56d386
HB04-23 <HB04-23@BIT04-19>
2018-05-02 20:17:28 +0900

    my벡터 만드릭기기기기기기

--- ch11/src/q2/MyVectorTest.java
+++ ch11/src/q2/MyVectorTest.java
@@ -10,8 +10,8 @@ class MyVector {
 		objArr = new Object[capacity]; // 생성자로 값을 받아서 배열의 크기를 설정한다.
 	}
 
-	public MyVector() {	//생성자 오버로딩 
-		this(16);	//다른 생성자를 불러온다.
+	public MyVector() { // 생성자 오버로딩
+		this(16); // 다른 생성자를 불러온다.
 	}
 
 	int size() {
@@ -24,17 +24,30 @@ class MyVector {
 		return objArr.length;
 	}
 
+	void setCapacity(int capacity) {
+		Object[] tmpObjArr = objArr; // 현재 객체배열을 임시로 담아둠
+		objArr = new Object[capacity]; // 새로운 객체배열 생성
+		for (int i = 0; i < size; i++) { // 새로 템프의 값을 담아준다.
+			try {
+				objArr[i] = tmpObjArr[i];
+			} catch (Exception e) { // 사이즈보다 적은 값이 들어오면 사이즈를 줄이고 넘치는 값을 없앤다.
+				this.size = i;
+				break; // 포문 탈출
+			}
+		}
+	}
+
 	boolean isEmpty() {
 		// 객체배열이 비었는지 반환
-		//return objArr[0] == null ? true : false; // 첫번째 배엘에 값이 없으면 뒤도 모두 값이 없다.
-		return size==0;
+		// return objArr[0] == null ? true : false; // 첫번째 배엘에 값이 없으면 뒤도 모두 값이 없다.
+		return size == 0;
 
 	}
 
 	void add(Object obj) {
 		// objArr에 객체를 추가하는 메서드
 		if (size < objArr.length)
-			objArr[size++] = obj;	//들어온 값을 넣어주고 size를 증가시킨다.
+			objArr[size++] = obj; // 들어온 값을 넣어주고 size를 증가시킨다.
 		else
 			System.out.println("배열의 범위를 넘어섰습니다.");
 	}
@@ -47,7 +60,7 @@ class MyVector {
 	public String toString() {
 		// objArr에 저장된 모든 객첼ㄹ 문자열로 이어서 반환
 		String tmp = "";
-		for (int i = 0; i < size; i++) 
+		for (int i = 0; i < size; i++)
 			tmp = tmp + objArr[i];
 		return tmp;
 	}
@@ -55,36 +68,36 @@ class MyVector {
 	int indexOf(Object obj) {
 		// 지정된 객체가 저장되어 있는 위치 반환
 		for (int i = 0; i < objArr.length; i++) {
-			if (objArr[i].equals(obj) )	//맞는 배열이 나오면 탈출
+			if (objArr[i].equals(obj)) // 맞는 배열이 나오면 탈출
 				return i;
 		}
-		return 0;
+		return -1;
 	}
 
 	boolean remove(Object obj) {
 		// indexOf()를 이용해서 지정된 객체 삭제
-		try {
-			if (size >= 0) {
-				int index = indexOf(obj);
-				objArr[index] = null;
-				for (int i = index; i < size; i++) {
-					try {
-						Object tmp = objArr[i];
-						objArr[i] = objArr[i + 1];
-						objArr[i + 1] = tmp;
-					} catch (Exception e) {
-						break;
-					}
+
+		int index = indexOf(obj); // 받은 값의 위치르 구한다. 없으면 -1이라 아래의 포문을 돌지 않음
+		if (index >= 0) {	
+			objArr[index] = null;	//인덱스의 값을 null로 바꾼다
+
+			for (int i = index; i < size; i++) {	//사이즈만큼 반복
+				try {
+					Object tmp = objArr[i];	//한칸씩 앞댕긴다.
+					objArr[i] = objArr[i + 1];
+					objArr[i + 1] = tmp;
+				} catch (Exception e) { //인덱스가 배열보다 크면 탈출
+					break;
 				}
-				size = size > 0 ? size - 1 : size;
-				return true;
-			} else
-				return false;
-		} catch (Exception e) {
-			return false;
-		}
+			}
+			
+			size = size > 0 ? size - 1 : size;	//사이즈가 0보다 크면 1만큼 출인다.
+			return true;
+		} else
+			return false;	//0보다 작으면 탈출(같은 객체가없으면);
 
 	}
+
 }
 
 class Test {
@@ -131,6 +144,11 @@ public class MyVectorTest {
 		System.out.println("2번 인덱스의 값 = " + my.get(2));
 		my.add(t1);
 		System.out.println("값 추가 후 = " + my);
+		my.setCapacity(4);
+		System.out.println("capacity = " + my.capacity());
+		System.out.println("크기 변경 후 = " + my);
+		System.out.println("삭제여부 = " + my.remove(t5));
+		System.out.println("값 삭제 후 = " + my);
 	}
 
 }
