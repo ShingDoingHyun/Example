dee9cfc95a8b06957c8321833e517c17b7268d03
hb2015 <hb2015@BIT04-15>
2018-05-03 17:38:06 +0900

    정렬

---
+++ ch11/src/comparatorEx/ComparatorEx.java
@@ -0,0 +1,36 @@
+package comparatorEx;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorEx {
+
+	public static void main(String[] args) {
+		String[] strArr = {"cat","Dog","lion","tiger"};
+		
+		Arrays.sort(strArr);
+		System.out.println("strArr="+Arrays.toString(strArr));
+		
+		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
+		System.out.println("strArr="+Arrays.toString(strArr));
+		
+		Arrays.sort(strArr, new Decendig());
+		System.out.println("strArr="+Arrays.toString(strArr));
+		
+
+	}
+
+}
+
+class Decendig implements Comparator{
+	
+	public int compare(Object o1, Object o2) {
+		if( o1 instanceof Comparable && o2 instanceof Comparable) {
+			Comparable c1 = (Comparable)o1;
+			Comparable c2 = (Comparable)o2;
+			return c1.compareTo(c2)*-1;
+			
+		}
+		return -1;
+	}
+}
