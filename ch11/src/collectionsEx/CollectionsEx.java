cb7f55299ba837ab19676aeaff3b639c18598c66
hb2015 <hb2015@BIT04-15>
2018-05-04 15:43:38 +0900

    콜렉션

--- ch11/src/collectionsEx/CollectionsEx.java
+++ ch11/src/collectionsEx/CollectionsEx.java
@@ -9,41 +9,41 @@ public class CollectionsEx {
 		List list = new ArrayList();
 		System.out.println(list);
 		
-		addAll(list,1,2,3,4,5);
+		addAll(list,1,2,3,4,5);//모든 객체삽입
 		System.out.println(list);
 		
-		rotate(list,2);
+		rotate(list,2);	//값만큼 이동
 		System.out.println(list);
 		
-		swap(list,0,2);
+		swap(list,0,2);	//값들의 위치 변경
 		System.out.println(list);
 		
-		shuffle(list);
+		shuffle(list);	//셔플
 		System.out.println(list);
 		
-		sort(list);
+		sort(list);	//기본정력
 		System.out.println(list);
 		
-		sort(list, reverseOrder());
+		sort(list, reverseOrder());	//정해진 방식으로 정렬
 		System.out.println(list);
 		
 		int idx = binarySearch(list,3);
-		System.out.println("index of 3 = "+ idx);
+		System.out.println("index of 3 = "+ idx);	//해당 인덱스의 값
 		
 		System.out.println("max="+max(list));
 		System.out.println("min="+min(list));
 		System.out.println("min="+max(list, reverseOrder()));
 		
-		fill(list,9);
+		fill(list,9);	//값으로 채운다
 		System.out.println("list="+list);
 		
-		List newList = nCopies(list.size(), 2);
+		List newList = nCopies(list.size(), 2);// 앞요소만큼 뒷요소의 값을 저장
 		System.out.println("newList="+newList);
 		
 		System.out.println(disjoint(list,newList));
 		
-		copy(list,newList);
-		System.out.println("newList="+newList);
+		copy(list,newList);	//복사
+		System.out.println("newList="+newList);	
 		System.out.println("list="+list);
 		
 		replaceAll(list,2,1);
